c2cb74bfd8809b1b693e5dfd98846e1e13e1aa53
Elvys <elvys_campos14@live.com>
2023-08-04 16:39:49 -0300

    render/fix/complete

--- back-end/src/data-source.ts
+++ back-end/src/data-source.ts
@@ -5,6 +5,16 @@ import "dotenv/config";
 const DataSourceConfig = (): DataSourceOptions => {
   const entitiesPath = path.join(__dirname, "entities/**.{js,ts}");
   const migrationsPath = path.join(__dirname, "migrations/**.{js,ts}");
+  const nodeEnv = process.env.NODE_ENV as string;
+
+  if (nodeEnv === "contactmanager") {
+    return {
+      type: "postgres",
+      url: process.env.DATABASE_URL!,
+      entities: [entitiesPath],
+      migrations: [migrationsPath],
+    };
+  }
 
   if (!process.env.DATABASE_URL) {
     throw new Error("Env var DATABASE_URL does not exists");
