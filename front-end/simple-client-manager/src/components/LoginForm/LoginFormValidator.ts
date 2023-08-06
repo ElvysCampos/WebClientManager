2bce69a94a6cf2bce6e2ab7ea4cd66a86275f661
Elvys <elvys_campos14@live.com>
2023-08-06 19:00:02 -0300

    begin-front-end

--- front-end/simple-client-manager/src/components/LoginForm/LoginFormValidator.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const loginSchema = z.object({
-    email: z.string().email("Deve ser um e-mail"),
-    password: z.string().nonempty("Senha é obrigatória"),
-});
-
-export type LoginData = z.infer<typeof loginSchema>;
