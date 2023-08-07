f096067bcf119879d01bb8815ab658ccf30d9c70
Elvys <elvys_campos14@live.com>
2023-08-06 21:09:29 -0300

    card/create

--- front-end/src/components/card.tsx
+++ front-end/src/components/card.tsx
@@ -1,5 +1,28 @@
-import {userSchema} from "@/schemas/users.schema"
+import { UserData } from "@/schemas/users.schema";
+import Image from "next/image";
 
 interface CardProps {
-    user: userSchema
+  user: UserData;
 }
+
+const Card = ({ user }: CardProps) => {
+  return (
+    <div className="flex flex-row justify-items-end bg-blue-900 w-72 h-64 rounded-lg">
+      <div className="flex flex-col items-center min-w-56">
+        <p>{user.name}</p>
+        <Image
+          className="m-4 mb-2 w-52 h-[11.5rem]"
+          width={209}
+          height={18}
+          src={user.name}
+          alt="imagem do user"
+        />
+      </div>
+      <div className="min-h-16 bg-slate-500 flex justify-center rounded-e-lg">
+        <button>colocar um icon demo 54.40 min encina</button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -1,14 +1,29 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { GetServerSideProps, NextPage } from "next";
+import { UserData } from "@/schemas/users.schema";
+import api from "@/services/api";
+import Card from "@/components/card";
 
-const inter = Inter({ subsets: ['latin'] })
+interface HomeProps {
+  user: UserData[];
+}
 
-export default function Home() {
+const Home: NextPage<HomeProps> = ({ user }) => {
   return (
     <main
-      className={`body flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`body flex min-h-screen flex-col items-center justify-between p-24`}
     >
-      <h1>treste</h1>
+      {user.map((users) => {
+        return <Card key={users.id} user={users} />;
+      })}
     </main>
-  )
-}
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await api.get<UserData[]>("/users");
+  return {
+    props: { user: response.data },
+  };
+};
+
+export default Home;
