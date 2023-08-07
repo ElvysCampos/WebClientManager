cb4a178ca02f5baa4a82e1dced93216fd93497b5
Elvys <elvys_campos14@live.com>
2023-08-07 02:49:44 -0300

    fix/cors

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,13 +1,23 @@
-import app from "./app";
+import express from "express";
+import cors from "cors";
 import AppDataSource from "./data-source";
-const PORT: number = 3000;
-const onMessage = `http://localhost:${PORT}`;
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "localhost:3001",
+    credentials: true,
+  })
+);
+
+const PORT = process.env.PORT || 3001;
 
 AppDataSource.initialize()
   .then(() => {
     console.log("Database Connected");
     app.listen(PORT, () => {
-      console.log(onMessage);
+      console.log(`Servidor rodando na porta ${PORT}`);
     });
   })
   .catch((error) => console.log(error));
